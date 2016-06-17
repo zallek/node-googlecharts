7e0bc56a0cc78cc9e6af76e37f780181d695bf71
zallek <nicolas.fortin37@gmail.com>
2016-06-17 15:49:12 +0200

    Remove log

--- bin/node-googlecharts.js
+++ bin/node-googlecharts.js
@@ -2,7 +2,7 @@ const nodeGoogleCharts = require('../src');
 
 
 const options = JSON.parse(process.argv[2]);
-console.log('options', options);
+
 nodeGoogleCharts(options)
 .then(console.log)
 .catch(console.error);
